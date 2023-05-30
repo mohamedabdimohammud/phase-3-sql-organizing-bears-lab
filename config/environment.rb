0bf1550a85cf61d34c0ee742fb2185a07d4f335f
mohamedabdimohammud <mmohammud@outlook.com>
2023-05-30 10:25:32 +0300

    Completed assignment

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,22 @@ require 'base64'
 Bundler.require
 
 require_relative '../lib/sql_queries'
+require 'sqlite3'
+
+# Connect to the database
+DB = SQLite3::Database.new("bears.db")
+
+# Create the bears table if it doesn't exist
+DB.execute <<-SQL
+  CREATE TABLE IF NOT EXISTS bears (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    sex TEXT,
+    age INTEGER,
+    alive BOOLEAN
+  );
+SQL
+
+# Load the SQL statements from insert.sql
+insert_sql = File.read(File.join(__dir__, 'lib', 'insert.sql'))
+DB.execute_batch(insert_sql)
